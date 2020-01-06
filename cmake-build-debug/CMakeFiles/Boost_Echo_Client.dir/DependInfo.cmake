7aa921b29eb58305b90615c31e026656d7903729
yairsep <yairsep@gmail.com>
2020-01-06 21:07:01 +0200

    Lab Commit 6.1.2020

---
+++ cmake-build-debug/CMakeFiles/Boost_Echo_Client.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/users/studs/bsc/2020/yairsep/Desktop/Boost_Echo_Client/src/connectionHandler.cpp" "/users/studs/bsc/2020/yairsep/Desktop/Boost_Echo_Client/cmake-build-debug/CMakeFiles/Boost_Echo_Client.dir/src/connectionHandler.cpp.o"
+  "/users/studs/bsc/2020/yairsep/Desktop/Boost_Echo_Client/src/echoClient.cpp" "/users/studs/bsc/2020/yairsep/Desktop/Boost_Echo_Client/cmake-build-debug/CMakeFiles/Boost_Echo_Client.dir/src/echoClient.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../bin"
+  "../include"
+  "../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Boost_Echo_Client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Boost_Echo_Client"
+  "Boost_Echo_Client.pdb"
+  "CMakeFiles/Boost_Echo_Client.dir/src/connectionHandler.cpp.o"
+  "CMakeFiles/Boost_Echo_Client.dir/src/echoClient.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Boost_Echo_Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
